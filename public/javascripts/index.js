fd1903bd8adf0aa120edd06cd8c5fa6290a14391
Steve Rubinstein <slrubinstein@gmail.com>
2014-10-09 10:33:31 -0400

    add a day functionality added

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,34 +1,6 @@
-function initialize_gmaps() {
- 
-  // initialize new google maps LatLng object
-  var myLatlng = new google.maps.LatLng(40.705786,-74.007672);
- 
-  // set the map options hash
-  var mapOptions = {
-    center: myLatlng,
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    zoom: 12
-  };
- 
-  // get the maps div's HTML obj
-  var map_canvas_obj = document.getElementById("map-canvas");
- 
-  // initialize a new Google Map with the options
-  var map = new google.maps.Map(map_canvas_obj, mapOptions);
- 
-  // Add the marker to the map
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    title:"Hello World!"
-  });
- 
-  // Add the marker to the map by calling setMap()
-  marker.setMap(map);
-}
+
 
 $(document).ready(function() {
-
 
   console.log('document is ready');
   initialize_gmaps();
@@ -41,30 +13,58 @@ $(document).ready(function() {
  
 // var days = [{'things': [], 'hotel': 'String', 'restaurants': [], day: num, markers: []}, {}, {}];
 
-var dayPlans = [{
+var dayPlans = [
   //must start with day 1
-  hotels: [], 
-  thingsToDo: [], 
-  restaurants: [], 
-  dayNum: 1 
-}];
+  // hotels: [], 
+  // thingsToDo: [], 
+  // restaurants: [], 
+  // markers: [], 
+  // dayNum: 1 
+];
 
-var currentDay = dayPlans[0]; 
+// var currentDay = dayPlans[0]; 
 
-function AddADay(){
+var currentDay; 
+
+var addADay = function(){
+
 
   var addedDay = {
     hotels: [], 
     thingsToDo: [], 
     restaurants: [], 
+    markers: [], 
     dayNum: dayPlans.length + 1
   }
 
-  var dayPlans.push(addedDay)
+  dayPlans.push(addedDay);
+  currentDay = dayPlans[addedDay.dayNum - 1]; 
+  // console.log('these are day plans', dayPlans); 
+  console.log('this is current day before daybutton', currentDay); 
+
+  $dayButton = $("<button type='button' class='btn btn-default dayButton'>Day " + addedDay.dayNum + "</button>"); 
+  // console.log(addedDay.dayNum); 
+  $dayButton.appendTo($("#daybtns")); 
+  $dayButton.on('click', function(event){
+    currentDay = dayPlans[addedDay.dayNum - 1]; 
+    console.log('new current day', currentDay); 
+    renderDays(); 
+
+  })
+  // event for that button 
+ 
+  // currentDay = dayPlans[addedDay.dayNum - 1]
+   // renderDays()
+
 
   
 }
 
+$("#addDay").on('click', function(event){
+  addADay(); 
+})
+
+  addADay(); 
 
 function renderDays(){
 
@@ -96,7 +96,7 @@ function renderDays(){
 
 $('.addButton').on('click', function(event){
   var thing = $(this).closest('.btn-group.inline').find('#nameField').text();
-  console.log('this things', thing); 
+  // console.log('this things', thing); 
   if (this.id === 'things'){ 
         currentDay.thingsToDo.push(thing);
         renderDays(); 
@@ -124,9 +124,9 @@ $('.addButton').on('click', function(event){
 
 // $(.removeButton).on('click', function(){})
 
-$('#addDay').on('click', function(){ 
-    addADay()
-});
+// $('#addDay').on('click', function(){ 
+//     addADay()
+// });
 
 
 // $('.dayButton').on('click', function(){
@@ -137,7 +137,34 @@ $('#addDay').on('click', function(){
 
 // });
 
-
+function initialize_gmaps() {
+ 
+  // initialize new google maps LatLng object
+  var myLatlng = new google.maps.LatLng(40.705786,-74.007672);
+ 
+  // set the map options hash
+  var mapOptions = {
+    center: myLatlng,
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 12
+  };
+ 
+  // get the maps div's HTML obj
+  var map_canvas_obj = document.getElementById("map-canvas");
+ 
+  // initialize a new Google Map with the options
+  var map = new google.maps.Map(map_canvas_obj, mapOptions);
+ 
+  // Add the marker to the map
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    title:"Hello World!"
+  });
+ 
+  // Add the marker to the map by calling setMap()
+  marker.setMap(map);
+}
 
 
 
